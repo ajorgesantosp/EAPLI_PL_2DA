2708f296e51fda67fb7602ea02abdf4bee1ae39c
RitaNogueira <ana_rita_nogueira@hotmail.com>
2013-04-04 17:08:11 +0100

    Criação da classe ShowExpenseUI, com a função de apresentar informações de uma despesa. Esta classe pode ser usada por várias outras classes de forma a reutlizar o código e uniformizar a apresentação de despesas.

---
+++ eapli.expensemanager/src/Presentation/ShowExpenseUI.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentation;
+
+import Model.Expense;
+
+/**
+ * 
+ * @author Paulo Gandra Sousa
+ */
+class ShowExpenseUI {
+	public static void show(Expense e) {
+		System.out.printf("Description: %s%s\nDate: %s\nAmount: %.2f€\n",
+				e.getDescription(), e.getType() == null ? "" : "\nType: "
+						+ e.getType().getDescription(), e.getDate(),
+				e.getAmount());
+	}
+}
