13cb785313d015089a1925ec5c1a927f432a79b1
gilfmc <gilfmcastro@gmail.com>
2013-04-05 15:08:03 +0100

    Criação da classe IncomeType

--- eapli.expensemanager/src/Model/IncomeType.java
+++ eapli.expensemanager/src/Model/IncomeType.java
@@ -7,31 +7,34 @@ package Model;
  */
 public class IncomeType {
 
-    private String description;
+   private String description;
 
-    public IncomeType(String description) {
-        this.description = description;
-    }
+   public IncomeType(String description) {
+      setDescription(description);
+   }
 
-    public IncomeType(IncomeType incomeType) {
-        this.description = incomeType.description;
-    }
+   public IncomeType(IncomeType incomeType) {
+      this.description = incomeType.description;
+   }
 
-    /**
-     * Gets the description of this IncomeType
-     *
-     * @return the description
-     */
-    public String getDescription() {
-        return description;
-    }
+   /**
+    * Gets the description of this IncomeType
+    *
+    * @return the description
+    */
+   public String getDescription() {
+      return description;
+   }
 
-    /**
-     * Sets the description of this IncomeType
-     *
-     * @param description the new description
-     */
-    public void setDescription(String description) {
-        this.description = description;
-    }
+   /**
+    * Sets the description of this IncomeType
+    *
+    * @param description the new description private String description; private Date date; BigDecimal amount; private
+    * ExpenseType type;
+    */
+   public void setDescription(String description) {
+      if (description == null)
+         throw new IllegalArgumentException();
+      this.description = description;
+   }
 }
