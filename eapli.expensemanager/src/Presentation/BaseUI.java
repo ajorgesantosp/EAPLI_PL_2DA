d8dd81d4f49b420b2434553f109af9ab73dde694
gilfmc <gilfmcastro@gmail.com>
2013-04-05 14:38:15 +0100

    Documentação da classe BaseUI

--- eapli.expensemanager/src/Presentation/BaseUI.java
+++ eapli.expensemanager/src/Presentation/BaseUI.java
@@ -1,6 +1,22 @@
 package Presentation;
 
+/**
+ * Classe abstracta necessária, principalmente, para as interfaces acessíveis
+ * directamente a partir do menu
+ *
+ * @author gilfmc & RitaNogueira
+ */
 public abstract class BaseUI {
+
+    /**
+     * Inicia a interface, este método é chamado quando o utilizador escolhe a
+     * opção associada a este
+     */
     public abstract void doShow();
+
+    /**
+     * Nome da acção associada a esta interface, utilizado no menu
+     * @return nome da acção
+     */
     public abstract String getActionName();
 }
