1e769cff8e07b4b15f74ff702653e0fecf1febda
RitaNogueira <ana_rita_nogueira@hotmail.com>
2013-04-05 15:23:11 +0100

    Correções de métodos

--- eapli.expensemanager/src/Model/PaymentMean.java
+++ eapli.expensemanager/src/Model/PaymentMean.java
@@ -1,25 +1,35 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
 /**
- *
+ * Represents a way of payment.This class is important in 
  * @author i110156
  */
 public class PaymentMean {
    
-    private String descricao;
+    private String description;
     
-    public PaymentMean(String descricao){
+    public PaymentMean(String description){
         
-        this.descricao=descricao;
+        this.description=description;
     }
     
     
     public String toString(){
         
-        return descricao;
+        return getDescription();
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description
+     */
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
