94baf57966a61c0eed9c57e78f280baf576aad68
Joao Camelo <Kameluh@Kameluh-PC>
2013-04-11 16:57:20 +0100

    ExpenseType & BaseType update

--- eapli.expensemanager/src/Model/ExpenseType.java
+++ eapli.expensemanager/src/Model/ExpenseType.java
@@ -6,11 +6,19 @@ package Model;
  * @author 1110119/1110285 (artur-kk / kameluh) + gilfmc & RitaNogueira
  */
 public class ExpenseType extends BaseType {   
-   public ExpenseType(int id, String description) {
+   
+    public ExpenseType(int id, String description) {
       super(id, description);
    }
    
    public ExpenseType(ExpenseType eT) {
       super(eT);
    }
+
+    @Override
+    public String toString() {
+        return "ExpenseType:\n" + super.toString() + ";";
+    }
+   
+   
 }
