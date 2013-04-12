3086ce7a15ef35333580b2ef97911257a3d65f35
i110687 <i110687@PCI087.dei.isep.ipp.pt>
2013-04-12 15:33:07 +0100

    getCurrentWeekExpenses now comunicates with ExpenseRecord

--- eapli.expensemanager/src/Controllers/WeekExpenditureController.java
+++ eapli.expensemanager/src/Controllers/WeekExpenditureController.java
@@ -5,13 +5,9 @@
 package Controllers;
 
 import Model.Expense;
-import Persistence.ExpenseRepository;
-import eapli.util.DateTime;
+import Model.ExpenseRecord;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import sun.util.calendar.CalendarDate;
 
 /**
  *
@@ -24,25 +20,9 @@ public class WeekExpenditureController {
     }
     
     public ArrayList<Expense> getCurrentWeekExpenses(){
-        //Calculo da data de inicio e de fim da semana em que se encontra
-        DateTime datetimer = new DateTime();
-        Calendar today = datetimer.today();
-        int current_month = datetimer.currentMonth();
-        int current_week = datetimer.currentWeekNumber();
-        int current_year = datetimer.currentYear();
-        Calendar first = datetimer.firstDateOfWeek(current_year, current_week), last = datetimer.lastDateOfWeek(current_year, current_week);
-        ExpenseRepository repo = new ExpenseRepository();
-        CalendarDate startDate=null,endDate=null;
-        startDate.setYear(current_year);
-        startDate.setMonth(current_month);
-        startDate.setDayOfMonth(first.DAY_OF_MONTH);
-        endDate.setYear(current_year);
-        endDate.setMonth(current_month);
-        endDate.setDayOfMonth(last.DAY_OF_MONTH);
-        //---------------------------------------------------------------
-        ArrayList<Expense> expenses;
-        expenses=repo.getExpenses(startDate,endDate);
-        return expenses;
+        ExpenseRecord er = new ExpenseRecord();
+        return er.getCurrentWeekExpenses();
+        
     }
     
     public BigDecimal getAmountWeekExpenses(ArrayList<Expense> weekExp){
