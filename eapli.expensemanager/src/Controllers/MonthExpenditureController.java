ab986b03531b98b83a3351830d4ffddea1939725
fabio <fabio@alpha>
2013-04-19 00:39:50 +0100

    Alteração da classe - 1080457

--- eapli.expensemanager/src/Controllers/MonthExpenditureController.java
+++ eapli.expensemanager/src/Controllers/MonthExpenditureController.java
@@ -2,59 +2,33 @@ package Controllers;
 
 import Model.Expense;
 import Model.ExpenseRecord;
-import Persistence.ExpenseRepository;
-import eapli.util.DateTime;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Calendar;
-import sun.util.calendar.CalendarDate;
 
+public class MonthExpenditureController extends BaseController {
 
-public class MonthExpenditureController {
- 
-    
-     public MonthExpenditureController(){
-        
+    ArrayList<Expense> expenses;
+
+    public MonthExpenditureController() {
     }
-         
-         
-    public ArrayList<Expense> getMonthExpenses(){
+
+    public ArrayList<Expense> getCurrentMonthExpenses() {
+        ExpenseRecord er = new ExpenseRecord();
+        return er.getCurrentMonthExpenses();
+    }
+
+    public ArrayList<Expense> getAnyMonthExpenses(int month, int year) {
+        ExpenseRecord er = new ExpenseRecord();
+        return er.getAnyMonthExpenses(month, year);
+    }
+
+    public BigDecimal getAmountMonthExpenses() {
         ExpenseRecord er = new ExpenseRecord();
         return er.getMonthExpenses();
     }
     
-       public ArrayList<Expense> getAnyMonthExpenses(){
+      public BigDecimal getAmountMonthExpenses(int month, int year) {
         ExpenseRecord er = new ExpenseRecord();
-        return er.getAnyMonthExpenses(month, year);
+        return er.getMonthExpenses(month, year);
     }
-    
-    public BigDecimal getAmountWeekExpenses(ArrayList<Expense> weekExp){
-        BigDecimal total=null;
-        for(Expense e:weekExp){
-            total=total.add(e.getAmount());
-        }
-        return total;
-    }
-     public BigDecimal getAmountAnyMonthExpenses(int month, int year){
-        BigDecimal total=null;
-        ArrayList<Expense> AnyMonthExp = getAnyMonthExpenses(month, year);
-        for(Expense e:AnyMonthExp){
-            total=total.add(e.getAmount());
-        }
-        return total;
-    }
-   
-
-    public BigDecimal getAmountMonthExpenses(ArrayList<Expense> monthExp){
-        BigDecimal total=null;
-        for(Expense e:monthExp){
-            total=total.add(e.getAmount());
-        }
-        return total;
-    }
-
 }
-    
-    
-
-
