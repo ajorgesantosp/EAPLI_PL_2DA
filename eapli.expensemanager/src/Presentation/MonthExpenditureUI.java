261be82b4b1167fb8948358aee945b8d038a4051
miniclip22 <miniclip22@ACDCROCKS-MINT-LAPTOP>
2013-04-19 22:36:41 +0100

    Removed unused imports and added some minor changes to the code.

--- eapli.expensemanager/src/Presentation/MonthExpenditureUI.java
+++ eapli.expensemanager/src/Presentation/MonthExpenditureUI.java
@@ -4,34 +4,36 @@ import Controllers.BaseController;
 import Controllers.MonthExpenditureController;
 import Model.Expense;
 
+/**
+ *
+ * @author AC/DC ROCKS!!!!
+ */
 public class MonthExpenditureUI extends BaseUI {
 
     MonthExpenditureController mec = new MonthExpenditureController();
 
-    public BaseController controller() {
-        return mec;
-    }
-
     public MonthExpenditureUI() {
     }
 
+    @Deprecated
     public void interfaceUI() {
         for (Expense e : mec.getCurrentMonthExpenses()) {
             System.out.println(e);
         }
     }
 
-    public void showBalance() {
-        System.out.println("Month balance:" + mec.getAmountMonthExpenses(mec.getCurrentMonthExpenses()));
-    }
-
     @Override
     public void doShow() {
-        showBalance();
+        System.out.print("Chosen Month balance: " + mec.getCurrentMonthExpenses());
     }
 
     @Override
     public String getActionName() {
-        return "Show Month Expenditure";
+        return "Show chosen Month Expenditure";
+    }
+
+    @Override
+    protected BaseController controller() {
+        return mec;
     }
 }
