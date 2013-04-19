f3315d3584f40b83f50c492c6ad2d5d10cc650f0
fabio <fabio@alpha>
2013-04-19 11:56:52 +0100

    correção erros   - 1080457

--- eapli.expensemanager/src/Controllers/MonthExpenditureController.java
+++ eapli.expensemanager/src/Controllers/MonthExpenditureController.java
@@ -17,18 +17,27 @@ public class MonthExpenditureController extends BaseController {
         return er.getCurrentMonthExpenses();
     }
 
-    public ArrayList<Expense> getAnyMonthExpenses(int month, int year) {
-        ExpenseRecord er = new ExpenseRecord();
-        return er.getAnyMonthExpenses(month, year);
-    }
-
-    public BigDecimal getAmountMonthExpenses() {
-        ExpenseRecord er = new ExpenseRecord();
-        return er.getMonthExpenses();
+    public BigDecimal getAmountMonthExpenses(ArrayList<Expense> monthExp) {
+        BigDecimal total = new BigDecimal(0);
+        for (Expense e : monthExp) {
+            total = total.add(e.getAmount());
+        }
+        return total;
     }
     
-      public BigDecimal getAmountMonthExpenses(int month, int year) {
-        ExpenseRecord er = new ExpenseRecord();
-        return er.getMonthExpenses(month, year);
-    }
+    /*
+     public ArrayList<Expense> getAnyMonthExpenses(int month, int year) {
+     ExpenseRecord er = new ExpenseRecord();
+     return er.getAnyMonthExpenses(month, year);
+     }
+
+     public BigDecimal getAmountMonthExpenses() {
+     ExpenseRecord er = new ExpenseRecord();
+     return er.getMonthExpenses();
+     }
+    
+     public BigDecimal getAmountMonthExpenses(int month, int year) {
+     ExpenseRecord er = new ExpenseRecord();
+     return er.getMonthExpenses(month, year);
+     }*/
 }
