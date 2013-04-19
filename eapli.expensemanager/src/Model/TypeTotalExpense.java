0924714400b594a364a9ffa6b506cc879d4da7f8
i110687 <i110687@PCI087.dei.isep.ipp.pt>
2013-04-19 14:43:47 +0100

    getMonthExpensesbyType

--- eapli.expensemanager/src/Model/TypeTotalExpense.java
+++ eapli.expensemanager/src/Model/TypeTotalExpense.java
@@ -8,15 +8,24 @@ import java.math.BigDecimal;
 
 /**
  *
- * @author i110687
+ * @author PIPOKID & noobshark
  */
 public class TypeTotalExpense {
-    private ExpenseType tipo;
-    private BigDecimal valor;
+    private ExpenseType type;
+    private BigDecimal value;
     
     public TypeTotalExpense(){
         
     }
     
+    public TypeTotalExpense(ExpenseType type, BigDecimal value){
+        this.type=type;
+        this.value=value;
+    }
     
+    public ExpenseType getType(){ return type;}
+    public BigDecimal getValue(){ return value;}
+    
+    public void setType(ExpenseType type){ this.type=type;}
+    public void setValue(BigDecimal value){ this.value=value;}
 }
