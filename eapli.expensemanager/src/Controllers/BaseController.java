3e8902459f4e63d07e02a4b50a130c143baa72e3
miniclip22 <miniclip22@ACDCROCKS-MINT-LAPTOP>
2013-04-19 22:31:03 +0100

    Removed getCurrentMonthExpenses() method.

--- eapli.expensemanager/src/Controllers/BaseController.java
+++ eapli.expensemanager/src/Controllers/BaseController.java
@@ -10,33 +10,7 @@ public abstract class BaseController {
 
     public BaseController() {
     }
-
-   public BigDecimal getCurrentMonthExpenses() {
-
-        ArrayList<Expense> expenses;
-        BigDecimal total;
-
-        ExpenseRepository rep = new ExpenseRepository();
-        expenses = rep.getCurrentMonthExpenses();
-
-        ExpenseRecord er = new ExpenseRecord();
-
-        total = er.getTotalAmountExpenses(expenses);
-
-        return total;
-    }
-        
-    public BigDecimal getMonthExpenses(int month, int year) {
-
-        ArrayList<Expense> expenses;
-        BigDecimal total;
-
-        ExpenseRepository rep = new ExpenseRepository();
-        expenses = rep.getAnyMonthExpenses(month, year);
-
-        ExpenseRecord er = new ExpenseRecord();
-        total = er.getTotalAmountExpenses(expenses);
-
-        return total;
-    }
 }
+
+   
+       
