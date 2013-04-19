36c5c823cc46b7d3cef6e099f9a4dabeb677d40e
fabio <fabio@alpha>
2013-04-19 13:00:46 +0100

    alteraçao metodos - 1080457

--- eapli.expensemanager/src/Controllers/BaseController.java
+++ eapli.expensemanager/src/Controllers/BaseController.java
@@ -1,16 +1,42 @@
 package Controllers;
 
+import Model.Expense;
 import Model.ExpenseRecord;
+import Persistence.ExpenseRepository;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public abstract class BaseController {
-
 
     public BaseController() {
     }
 
-    public BigDecimal getMonthExpenditure() {
+   public BigDecimal getCurrentMonthExpenses() {
+
+        ArrayList<Expense> expenses;
+        BigDecimal total;
+
+        ExpenseRepository rep = new ExpenseRepository();
+        expenses = rep.getCurrentMonthExpenses();
+
         ExpenseRecord er = new ExpenseRecord();
-        return er.getMonthExpenses();
+
+        total = er.getTotalAmountExpenses(expenses);
+
+        return total;
+    }
+        
+    public BigDecimal getMonthExpenses(int month, int year) {
+
+        ArrayList<Expense> expenses;
+        BigDecimal total;
+
+        ExpenseRepository rep = new ExpenseRepository();
+        expenses = rep.getAnyMonthExpenses(month, year);
+
+        ExpenseRecord er = new ExpenseRecord();
+        total = er.getTotalAmountExpenses(expenses);
+
+        return total;
     }
 }
