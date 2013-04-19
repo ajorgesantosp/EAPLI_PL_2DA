1a6a465db461593fa96c7815319952d44d317f0e
fabio <fabio@alpha>
2013-04-19 11:49:07 +0100

    Alteração da classe - 1080457

--- eapli.expensemanager/src/Presentation/MonthExpenditureUI.java
+++ eapli.expensemanager/src/Presentation/MonthExpenditureUI.java
@@ -2,46 +2,36 @@ package Presentation;
 
 import Controllers.BaseController;
 import Controllers.MonthExpenditureController;
-import Controllers.WeekExpenditureController;
 import Model.Expense;
-import java.math.BigDecimal;
 
+public class MonthExpenditureUI extends BaseUI {
 
-public class MonthExpenditureUI  extends BaseUI{
-   /*public void interfaceUI(){
-        MonthExpenditureController mecontroller = new MonthExpenditureController();
-        for(Expense e : mecontroller.getCurrentMonthExpenses()){
-            System.out.println(e);
-        }
-   }*/
-       MonthExpenditureController mec = new MonthExpenditureController();
-    
-    public MonthExpenditureUI(){
-        
+    MonthExpenditureController mec = new MonthExpenditureController();
+
+    public BaseController controller() {
+        return mec;
     }
-    
-    public void interfaceUI(){
-        for(Expense e : mec.getMonthExpenses()){
+
+    public MonthExpenditureUI() {
+    }
+
+    public void interfaceUI() {
+        for (Expense e : mec.getCurrentMonthExpenses()) {
             System.out.println(e);
         }
     }
-    
-    public void showBalance(){
-        System.out.println("Month balance:" + mec.getAmountMonthExpenses(mec.getMonthExpenses()));
+
+    public void showBalance() {
+        System.out.println("Month balance:" + mec.getAmountMonthExpenses(mec.getCurrentMonthExpenses()));
     }
-    
-    
 
     @Override
     public void doShow() {
-         showBalance(); 
+        showBalance();
     }
 
     @Override
     public String getActionName() {
-    return "Show Month Expenditure";    
+        return "Show Month Expenditure";
     }
-
 }
-    
-
